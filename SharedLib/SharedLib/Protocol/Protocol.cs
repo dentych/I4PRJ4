105850609b44e005b1ac94da54c3c9826767b632
Jakob Kristensen <j4k0bk@gmail.com>
2015-09-24 17:25:38 +0200

    Implemented Protocol.cs

--- SharedLib/SharedLib/Protocol/Protocol.cs
+++ SharedLib/SharedLib/Protocol/Protocol.cs
@@ -9,5 +9,22 @@ namespace SharedLib.Protocol
     public class Protocol
     {
         private IProtocolMarshal _marshaller = new XmlMarshal();
+        private XmlBuffer _buffer = new XmlBuffer();
+
+        public void AddData(string data)
+        {
+            _buffer.AddData(data);
+        }
+
+        public IEnumerable<Command> GetCommands()
+        {
+            foreach (var doc in _buffer.GetDocuments())
+                yield return _marshaller.Decode(doc);
+        }
+
+        public string Encode(Command cmd)
+        {
+            return _marshaller.Encode(cmd);
+        }
     }
 }
