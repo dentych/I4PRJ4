bbb5f1cef6be0146cbe7145857f10c4179d5cefa
Jakob Kristensen <j4k0bk@gmail.com>
2015-10-23 22:40:45 +0200

    Added unit test for XmlBuffer

---
+++ SharedLib/SharedLib.UnitTest/XmlBufferUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using SharedLib.Protocol;
+
+
+namespace SharedLib.UnitTest
+{
+    [TestFixture]
+    public class XmlBufferUnitTest
+    {
+        [Test]
+        public void Encode_MarshalEncodeCalled()
+        {
+            var s = "<Command Name=\"Demo\" />";
+            var buffer = new XmlBuffer();
+            var docs = buffer.GetDocuments();
+            docs.ToList();
+
+            buffer.AddData(s);
+
+            Assert.That(docs.First, Is.EqualTo(s));
+        }
+    }
+}
