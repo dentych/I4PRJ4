8ddf2f555399412599a3092c53858ded29bea19f
Benjamin Larsen <benlarsendk@gmail.com>
2015-10-26 21:19:15 +0100

    Started on editproduct which is crap

---
+++ Backend/Backend/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Models;
+using SharedLib.Models;
+
+namespace Backend.ViewModels
+{
+    class EditProductViewModel
+    {
+        public EditProductViewModel(Product toe, BackendProductCategory cat, BackendProductCategoryList catlist)
+        {
+            ProductToEdit = toe;
+            ProductCategory = cat;
+            CatList = catlist;
+
+        }
+
+        public Product ProductToEdit { get; set; }
+        public BackendProductCategory ProductCategory { get; set; }
+        public Product EditedProduct { get; set; }
+        public BackendProductCategoryList CatList { get; set; }
+
+    }
+}
