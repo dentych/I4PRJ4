088023f863e280d81f329e962e9567e9465cbd12
Benjamin Larsen <benlarsendk@gmail.com>
2015-11-09 14:38:46 +0100

    Subscribemethods and productcatshandlers

--- Backend/Backend/Models/SocketEvents/SocketEventHandlers.cs
+++ Backend/Backend/Models/SocketEvents/SocketEventHandlers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Backend.Communication;
+using SharedLib.Models;
 using SharedLib.Protocol.Commands;
 using SharedLib.Protocol.Commands.ProductCategoryCommands;
 
@@ -24,7 +25,7 @@ namespace Backend.Models.SocketEvents
 
         public void ProductDeletedHandler(ProductDeletedCmd product)
         {
-            this._categories
+           // this._categories
         }
 
         public void ProductEditedHandler(ProductEditedCmd product)
@@ -42,17 +43,38 @@ namespace Backend.Models.SocketEvents
 
         public void ProductCategoryCreatedHandler(ProductCategoryCreatedCmd category)
         {
-            throw new NotImplementedException();
+            var cat = new BackendProductCategory()
+            {
+                BName = category.Name,
+                ProductCategoryId = category.ProductCategoryId,
+                Products = category.Products
+            };
+
+            _categories.Add(cat);
         }
 
         public void ProductCategoryDeletedHandler(ProductCategoryDeletedCmd category)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < _categories.Count; i++)
+            {
+                if (_categories[i].ProductCategoryId == category.ProductCategoryId)
+                {
+                    _categories.RemoveAt(i);
+                    break;
+                }
+            }
         }
 
-        public void ProductCategoryEditedHandler(ProductCategoryEditedCmd product)
+        public void ProductCategoryEditedHandler(ProductCategoryEditedCmd category)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < _categories.Count; i++)
+            {
+                if (_categories[i].ProductCategoryId == category.ProductCategoryId)
+                {
+                    _categories[i].BName = category.Name;
+                   // _categories[i].Products = category.Products; 
+                }
+            }
         }
 
         public void SubscribeProductCreated()
@@ -62,12 +84,12 @@ namespace Backend.Models.SocketEvents
 
         public void SubscribeProductDeleted()
         {
-            throw new NotImplementedException();
+            LSC.Listener.OnProductDeleted += ProductDeletedHandler;
         }
 
         public void SubscribeProductEdited()
         {
-            throw new NotImplementedException();
+            LSC.Listener.OnProductEdited += ProductEditedHandler;
         }
 
         public void SubscribeCatalogueDetails()
@@ -77,17 +99,17 @@ namespace Backend.Models.SocketEvents
 
         public void SubscribeProductCategoryCreated()
         {
-            throw new NotImplementedException();
+            LSC.Listener.OnProductCategoryCreated += ProductCategoryCreatedHandler;
         }
 
         public void SubscribeProductCategoryDeleted()
         {
-            throw new NotImplementedException();
+            LSC.Listener.OnProductCategoryDeleted += ProductCategoryDeletedHandler;
         }
 
         public void SubscribeProductCategoryEdited()
         {
-            throw new NotImplementedException();
+            LSC.Listener.OnProductCategoryEdited += ProductCategoryEditedHandler;
         }
 
         #endregion
