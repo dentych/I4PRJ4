6c3184bfed7d6cf381c79da3759aa7b300ea7e43
Jakob Kristensen <j4k0bk@gmail.com>
2015-11-16 01:47:58 +0100

    New DB context and migration

---
+++ CentralServer/CentralServer/Migrations/DatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using SharedLib.Models;
+
+namespace CentralServer.Database
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("LocalDBv11") { }
+
+        public DbSet<PurchasedProduct> PurchasedProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+
+        public Product CreateProduct(string name, string productNumber, decimal price, int categoryId)
+        {
+            var product = new Product()
+            {
+                Name = name,
+                ProductNumber = productNumber,
+                Price = price,
+                ProductCategoryId = categoryId,
+            };
+
+            Products.Add(product);
+
+            return product;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasRequired(p => p.ProductCategory)
+                .WithMany(b => b.Products)
+                .HasForeignKey(p => p.ProductCategoryId);
+        }   
+    }
+}
