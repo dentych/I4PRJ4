f69d6d84343612f1e014ba418c1844396b77fed7
Andreas <andreas_larsen408@hotmail.com>
2015-11-25 13:37:38 +0100

    Added ProductCategory Testfiles

---
+++ SharedLib/SharedLib.UnitTest/CmdMarshallers.UnitTest/ProductCategoryUnitTest/CreateProductCategoryMarshalUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib.UnitTest.CmdMarshallers.UnitTest.ProductCategoryUnitTest
+{
+    class CreateProductCategoryMarshalUnitTest
+    {
+    }
+}
