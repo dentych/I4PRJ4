82e7cc040ac6afcbdba250c6e06d00cb0dbd38a6
KasperCK <kapperck@gmail.com>
2015-11-26 09:50:48 +0100

    Kasse kvitering

---
+++ KasseApparat/KasseApparat/IPrinter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SharedLib.Models;
+
+namespace KasseApparat
+{
+    public interface IPrinter
+    {
+        void PrintPurchase(List<PurchasedProduct> shoplist);
+    }
+}
